0e108d5c3a2b27d53353af16f7c42e779e28e478
James Dizon <dizonjerlin@gmail.com>
2024-02-12 14:50:26 -0800

    Completed Pt 1

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "PI";
+let continent = "Asia";
+let population = "1 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = 'amazing';
+
+console.log('Hello');
+console.log('3');
+let firstName = 'James';
+
+console.log(firstName);
