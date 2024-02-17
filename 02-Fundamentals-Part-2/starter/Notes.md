b87b852dcb80c1308bb9929ddd082f2d51c45560
James Dizon <dizonjerlin@gmail.com>
2024-02-16 20:24:41 -0800

    Updated Files

--- 02-Fundamentals-Part-2/starter/Notes.md
+++ 02-Fundamentals-Part-2/starter/Notes.md
@@ -15,3 +15,10 @@
 
 ### Javascript has dynamic type: We do not have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically
 
+## To do a string literal, you must use this `Add code here` 
+ie. 
+const james = "I'm  " + firstName + ", a " + (year-birthYear) + " years old " + job;
+console.log(james);
+
+const jamesNew = ` I'm ${firstName}! I'm a ${year - birthYear} years old ${job}`;
+console.log(jamesNew);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,29 @@ let BMIJohn = massJohn / (heightJohn * heightJohn);
 let markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// Part 2 Challenge
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// /* Write your code below. Good luck! 🙂 */
+// console.log("Mark's BMI (" + BMIMark.toFixed(2)  +") is higher than John's (" + BMIJohn.toFixed(2) +")!")
+
+
+// String Literals
+// let firstName = 'James';
+// let job = 'Developer';
+// let birthYear = 1994;
+// const year = 2024;
+
+// const james = "I'm  " + firstName + ", a " + (year-birthYear) + " year old " + job;
+// console.log(james);
+
+// const jamesNew = ` I'm ${firstName}! I'm a ${year - birthYear} year old ${job}`;
+// console.log(jamesNew);
